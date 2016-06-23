96fda162a385f115236b3ee6ea0f6f05d9dd65eb
awebdesigner09 <awebdesigner09@gmail.com>
2016-06-23 22:17:18 +0530

    Added Stylable Tab

---
+++ Window1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+namespace WPFDynamicTab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Window1 : MetroWindow
+    {
+        public Window1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            string tag = ((Button)sender).Tag.ToString();
+            switch (tag)
+            {
+                case "min": this.WindowState = WindowState.Minimized; break;
+                case "close": Application.Current.Shutdown(); break;
+            }
+        }
+
+
+    }
+}
